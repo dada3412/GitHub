d104dd3d962cdb7a14001a845d619ce814531fdd
Nico <nico@NicodeMac-mini.local>
2015-05-31 22:41:57 +0800

    Initial Commit

---
+++ 11_PopoverEdotor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  11_PopoverEdotor
+//
+//  Created by Nico on 15/5/31.
+//  Copyright (c) 2015年 Nico. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 11_PopoverEdotor/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  11_PopoverEdotor
+//
+//  Created by Nico on 15/5/31.
+//  Copyright (c) 2015年 Nico. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
