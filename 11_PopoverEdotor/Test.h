7f763b7d38717ac70e2d8ae906628dc2821f2ad2
Niko Chen <547642984@qq.com>
2016-04-16 01:35:52 +0800

    V1.2Test

---
+++ 11_PopoverEdotor/Test.h
@@ -0,0 +1,13 @@
+//
+//  Test.h
+//  11_PopoverEdotor
+//
+//  Created by Nico on 16/4/16.
+//  Copyright © 2016年 Nico. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test : NSObject
+@property(strong,nonatomic)NSString *str;
+@end
---
+++ 11_PopoverEdotor/Test.m
@@ -0,0 +1,13 @@
+//
+//  Test.m
+//  11_PopoverEdotor
+//
+//  Created by Nico on 16/4/16.
+//  Copyright © 2016年 Nico. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
+@end
